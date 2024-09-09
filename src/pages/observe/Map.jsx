121601df5a7fb2e179ec1ace29444b5a47aaff41
Manh ND <manh16nd@proton.me>
2024-09-09 23:02:23 +0700

    build

--- src/pages/observe/Map.jsx
+++ src/pages/observe/Map.jsx
@@ -10,16 +10,19 @@ function getWindowDimensions() {
   }
 }
 
-export default function Map({ gps }) {
+function MapControl({ gps }) {
   const map = useMap()
-
-  const myGps = gps?.x && gps?.y ? [gps.x, gps.y] : null
-
   useEffect(() => {
     if (gps?.x && gps?.y) {
       map.setView([gps.x, gps.y], map.getZoom())
     }
   }, [gps])
+
+  return null
+}
+
+export default function Map({ gps }) {
+  const myGps = gps?.x && gps?.y ? [gps.x, gps.y] : null
 
   const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions())
 
@@ -36,6 +39,7 @@ export default function Map({ gps }) {
     <div className="container mx-auto p-1">
       <MapContainer center={myGps ? myGps : [21.574674568874638, 105.81164980996522]} zoom={13} scrollWheelZoom={false} style={{ width: windowDimensions.width * 0.7, height: windowDimensions.height / 2 }}>
         <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <MapControl gps={gps} />
         {myGps && (
           <Marker position={myGps}>
             <Popup>
