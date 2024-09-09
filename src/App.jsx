ad5c6070f35f59122930bcd83eab4d9de92bc503
Manh ND <manh16nd@proton.me>
2024-09-09 22:35:48 +0700

    build

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Observe from './pages/observe/Observe'
 
 function App() {
   return (
-    <div className="">
+    <div className="min-h-screen bg-slate-100">
       <Observe />
       <Map />
     </div>
