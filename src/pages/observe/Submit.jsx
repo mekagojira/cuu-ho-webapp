6c661d55407a8617e7b2bb110e2cfd27f1dfcc73
Manh ND <manh16nd@proton.me>
2024-09-09 22:41:04 +0700

    bui

--- src/pages/observe/Submit.jsx
+++ src/pages/observe/Submit.jsx
@@ -5,11 +5,37 @@ export function Submit() {
   const regions = ['Thái Nguyên', 'Phú Thọ']
   const [region, setRegion] = useState(regions[0])
 
+  const getGps = () => {
+    if (navigator.geolocation) {
+      // get the current users location
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          // save the geolocation coordinates in two variables
+          const { latitude, longitude } = position.coords
+          // update the value of userlocation variable
+          alert(JSON.stringify({ latitude, longitude }))
+        },
+        // if there was an error getting the users location
+        error => {
+          console.error('Error getting user location:', error)
+        }
+      )
+    }
+    // if geolocation is not supported by the users browser
+    else {
+      console.error('Geolocation is not supported by this browser.')
+    }
+  }
+
   return (
     <div className="p-2">
       <h1 className="font-bold text-3xl uppercase">SOS-Thái Nguyên</h1>
       <div className="py-4" />
-      <form className="block">
+      <form
+        className="block"
+        onSubmit={e => {
+          e.preventDefault()
+        }}>
         <label htmlFor="input" className="text-slate-400 font-bold">
           Địa chỉ cụ thể (số người , nước dâng cao tới đâu, nhà mấy tầng, mô tả địa chỉ).
         </label>
@@ -24,6 +50,10 @@ export function Submit() {
           ))}
           <input placeholder="Nhập khu vực của bạn" className="px-3 py-2 rounded border border-slate-500" />
         </div>
+        <div className="py-2" />
+        <button onClick={getGps} className="rounded font-bold px-3 py-2 bg-slate-200">
+          LẤY THÔNG TIN TỌA ĐỘ
+        </button>
       </form>
     </div>
   )
