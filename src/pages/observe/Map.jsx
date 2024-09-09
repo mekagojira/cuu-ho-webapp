08b9bf14cb4a6c326734afa6c810837ee534dc21
Manh ND <manh16nd@proton.me>
2024-09-09 22:53:43 +0700

    build

--- src/pages/observe/Map.jsx
+++ src/pages/observe/Map.jsx
@@ -26,7 +26,7 @@ export default function Map({ gps }) {
 
   return (
     <div className="container mx-auto p-4">
-      <MapContainer center={[21.574674568874638, 105.81164980996522]} zoom={13} scrollWheelZoom={false} style={{ width: windowDimensions.width * 0.7, height: windowDimensions.height / 2 }}>
+      <MapContainer center={myGps ? myGps : [21.574674568874638, 105.81164980996522]} zoom={13} scrollWheelZoom={false} style={{ width: windowDimensions.width * 0.7, height: windowDimensions.height / 2 }}>
         <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         {myGps && (
           <Marker position={myGps}>
