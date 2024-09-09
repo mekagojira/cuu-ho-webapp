ba67654f9a444106614e2e23fd1c8aae9f8aa4db
Manh ND <manh16nd@proton.me>
2024-09-09 22:34:59 +0700

    build

--- src/pages/observe/Map.jsx
+++ src/pages/observe/Map.jsx
@@ -4,7 +4,7 @@ import 'leaflet/dist/leaflet.css'
 export default function Map() {
   return (
     <div className="container mx-auto p-2a">
-      <MapContainer center={[21.574674568874638, 105.81164980996522]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={[21.574674568874638, 105.81164980996522]} zoom={13} scrollWheelZoom={false} style={{ width: 600, height: 600 }}>
         <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       </MapContainer>
     </div>
