db21e43a5264e146eea2e16b89f47a7e3fb30f3c
Manh ND <manh16nd@proton.me>
2024-09-09 23:32:52 +0700

    build

---
+++ src/lib/request/request.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const HOST = 'https://api.congdongcuuho.vn'
+
+const call = async opts => {
+  try {
+    const { data: response } = await axios(call)
+
+    return response
+  } catch (e) {
+    alert(e.message)
+  }
+}
+
+export const listArticles = ({ keyword, status, page = 1, pageSize = 50 }) =>
+  call({
+    method: 'get',
+    url: `${HOST}/article`,
+    params: { keyword, status, page, pageSize },
+  })
