054dbd13bea20986a38bc02de79bf1b22144ef34
Manh ND <manh16nd@proton.me>
2024-09-09 22:34:34 +0700

    build

--- src/pages/observe/Submit.jsx
+++ src/pages/observe/Submit.jsx
@@ -7,13 +7,14 @@ export function Submit() {
 
   return (
     <div className="p-2">
-      <h1 className="font-bold text-lg uppercase">Đăng tin</h1>
+      <h1 className="font-bold text-lg uppercase">SOS-Thái Nguyên</h1>
       <form className="block">
         <label htmlFor="input" className="text-slate-400 font-bold">
-          Nhập thông tin của bạn (số người , nước dâng cao tới đâu, nhà mấy tầng, mô tả địa chỉ).
+          Địa chỉ cụ thể (số người , nước dâng cao tới đâu, nhà mấy tầng, mô tả địa chỉ).
         </label>
         <input id="input" className="w-full px-3 py-2 rounded border " placeholder="Nhập thông tin cứu hộ" />
         <div className="py-2" />
+        <div>Khu vực bạn ở?</div>
         <div className="flex space-x-4 flex-wrap">
           {regions.map((item, i) => (
             <button key={i} className={classNames('font-bold rounded uppercase px-3 py-2 border', region === item ? 'bg-red-500 border-red-500 text-white' : 'bg-white text-black border-slate-500')} onClick={() => setRegion(item)}>
